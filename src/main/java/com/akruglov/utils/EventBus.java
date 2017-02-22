bc7fd4d6fb5db74cfb7e3df71e4339b951801ac9
Andrey Kruglov <kruglov.it@gmail.com>
2017-02-22 09:37:41 +0300

    Javadoc documentation.

--- src/main/java/com/akruglov/utils/EventBus.java
+++ src/main/java/com/akruglov/utils/EventBus.java
@@ -6,6 +6,14 @@ import java.util.Set;
 import java.util.function.Consumer;
 
 
+/**
+ * Implementation of event bus.
+ *
+ * Event bus can send events to all its subscribers.
+ * Subscribers can be various objects or static methods, or lambdas.
+ *
+ * @author Andrey Kruglov
+ */
 public class EventBus {
     
     // For static methods and lambdas
@@ -20,7 +28,14 @@ public class EventBus {
         registrations = HashMultimap.create();
         consumers = HashMultimap.create();
     }
-    
+
+    /**
+     * Susbscribes an object to an event.
+     *
+     * @param eventClass class of event to subscribe
+     * @param listener object to receive events of given class
+     * @param consumer reference to method of the object that will handle events of given class
+     */
     public <Event, Listener> void subscribe(Class<Event> eventClass,
             Listener listener, Consumer<Event> consumer) {
         
@@ -32,7 +47,16 @@ public class EventBus {
         
         consumersForListener.add(consumer);
     }
-    
+
+    /**
+     * Subsribes static method or lamda to an event.
+     *
+     * NOTE: References to static methods or/and lambdas must be cached
+     * by caller to unsubscribe in the future.
+     *
+     * @param eventClass class of event to subscribe
+     * @param freeConsumer reference to static method or lambda that will handle events of given class
+     */
     public <Event> void subscribe(Class<Event> eventClass,
             Consumer<Event> freeConsumer) {
         
@@ -40,7 +64,16 @@ public class EventBus {
         
         consumersForEvent.add(freeConsumer);
     }
-    
+
+    /**
+     * Unsusbcribes an object from an event.
+     *
+     * Removes all handlers of the object that handle the event of given class.
+     *
+     * @param eventClass class of event to unsubscribe
+     * @param listener object to unsubscribe from events of given class
+     * @throws EventBusSubscriptionNotFoundException if given object is not subscribed to events of given class
+     */
     public <Event, Listener> void unsubscribe(Class<Event> eventClass,
             Listener listener) throws EventBusSubscriptionNotFoundException {
         
@@ -56,7 +89,15 @@ public class EventBus {
             registrations.removeAll(eventClass);
         }
     }
-    
+
+    /**
+     * Unsubsribes a static method or lambda from an event.
+     *
+     * @param eventClass class of event to unsubscribe
+     * @param freeConsumer reference to static method or lambda to unsubscribe from events of given class
+     * @throws EventBusSubscriptionNotFoundException if given static method or lambda is not subscribed to
+     *     event of given class
+     */
     public <Event> void unsubscribe(Class<Event> eventClass,
             Consumer<Event> freeConsumer) throws EventBusSubscriptionNotFoundException {
         
@@ -70,7 +111,12 @@ public class EventBus {
             freeConsumers.removeAll(eventClass);
         }
     }
-    
+
+    /**
+     * Notify all subscribed objects, static methods and lambdas about given event
+     *
+     * @param event event to notify
+     */
     public <Event> void fire(Event event) {
         notifyFreeConsumers(event);        
         notifyRegisteredObjects(event);
